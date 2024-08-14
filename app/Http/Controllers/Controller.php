c1831606d3bec1cb1ebf63e4845c9720a466e8d0
tamachine <tamachine@DESKTOP-BR4S63G.local>
2024-08-14 09:18:26 +0200

    update verifyContentType to receive the content type

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -7,16 +7,21 @@ use Illuminate\Http\Response;
 
 abstract class Controller
 {
+
+    const CONTENT_TYPE_APPLICATION_JSON = 'application/json';
+    CONST CONTENT_TYPE_APPLICATION_FORM = 'application/x-www-form-urlencoded';
+
      /**
      * Verify that the Content-Type header is application/json.
      *
      * @param \Illuminate\Http\Request $request
+     * @param String $contentType
      * @return \Illuminate\Http\Response|null
      */
-    protected function verifyContentType(Request $request)
+    protected function verifyContentType(Request $request, String $contentType = self::CONTENT_TYPE_APPLICATION_JSON)
     {
         // Verify content-type
-        if ($request->header('Content-Type') !== 'application/json') {
+        if ($request->header('Content-Type') !== $contentType) {
             return response()->json([
                 'error' => 'Invalid content type'
             ], Response::HTTP_UNSUPPORTED_MEDIA_TYPE);
@@ -24,4 +29,6 @@ abstract class Controller
 
         return null; // No error, proceed with the request
     }
+
+    
 }
